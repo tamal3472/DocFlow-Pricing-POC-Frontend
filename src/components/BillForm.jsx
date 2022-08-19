70b5721962ef7fc268f9452e63cc04de8c88f420
tamal3472 <tamal3472@gmail.com>
2022-08-19 13:56:19 +0600

    chore: Complete Structure

--- src/components/BillForm.jsx
+++ src/components/BillForm.jsx
@@ -1,12 +1,40 @@
-function BillForm() {
+import { useState, useEffect } from 'react';
+import axios from "axios";
+
+const BillForm = ({ selectedPlan, subscriptionType }) => {
+  const [numberOfUsers, setNumberOfUsers] = useState(selectedPlan.user_limit);
+  const [totalBill, setTotalBill] = useState();
+
+  const calculateBill = async () => {
+    const { data } = await axios.get(`http://localhost:3000/plans/${selectedPlan.id}/calculate_bill.json?number_of_user=${numberOfUsers}&subscription_type=${subscriptionType}`);
+    setTotalBill(data.calculated_amount);
+  };
+
+  useEffect(() => {
+    setNumberOfUsers(selectedPlan.user_limit);
+    setTotalBill();
+  }, [selectedPlan, subscriptionType]);
+
   return (
     <div className="mt-10 text-right">
-      <h1 className="text-2xl">You've selected solo</h1>
+      <h1 className="text-2xl">You've selected {selectedPlan.name}</h1>
       <div className="mt-4">
         <label className="text-sm">Number of users</label>
-        <input type="text" className="ml-3 p-1 rounded border-gray-300" />
+        <input
+          type="text"
+          className="ml-3 p-1 rounded border-gray-300"
+          value={numberOfUsers}
+          onChange={evt => setNumberOfUsers(evt.target.value)}
+          disabled={selectedPlan.name === 'solo'}
+        />
+        <button
+          className="ml-6 mt-10 bg-green-500 px-4 py-2 text-white rounded text-sm hover:bg-green-400 hover:text-gray-200"
+          onClick={calculateBill}
+        >
+          Calculate Bill
+        </button>
       </div>
-      <h2 className="text-lg mt-4">Total price: $60</h2>
+      <h2 className="text-lg mt-4">Total price: ${totalBill} / Month</h2>
     </div>
   )
 }
--- src/components/PriceTable.jsx
+++ src/components/PriceTable.jsx
@@ -1,72 +1,54 @@
-import BillForm from './BillForm'
+import { useState, useEffect } from 'react';
+import axios from "axios";
+import BillForm from './BillForm';
 
-function PricingTable({ payPerMonth }) {
+
+const PricingTable = ({ payPerMonth }) => {
+  const [plans, setPlans] = useState([]);
+  const [selectedPlan, setSelectedPlan] = useState();
+
+  const fetchPlans = async () => {
+    const { data } = await axios.get('http://localhost:3000/plans.json');
+    setPlans(data.plans);
+  };
+
+  const renderPrice = plan => {
+    if (payPerMonth) {
+      return `$${plan.price_monthly} / Month`;
+    }
+
+    return `$${plan.price_yearly} / Month`;
+
+  }
+
+  useEffect(() => {
+    fetchPlans();
+  }, []);
+
   return (
     <>
       <div className="mt-10 grid gap-3 grid-cols-3">
-        {
-          payPerMonth &&
-          <div className="rounded shadow-md p-4">
-            <h1 className="text-3xl">Solo</h1>
-            <h2 className="text-2xl mt-4">$6 / {payPerMonth ? 'month' : 'year'}</h2>
+        { plans.map(plan =>(
+          <div key={plan.id} className="rounded shadow-md p-4">
+            <h1 className="capitalize text-3xl">{plan.name}</h1>
+            <h2 className="text-2xl mt-4">{renderPrice(plan)}</h2>
             <div className="flex justify-between px-5 mt-10">
               <p className="text-base font-light">Template</p>
-              <p className="text-sm">1</p>
+              <p className="text-sm">{ plan.name == 'solo' ? 1 : 'Unlimited' }</p>
             </div>
             <div className="flex justify-between px-5 mt-2">
               <p className="text-base font-light">Users</p>
-              <p className="text-sm">1</p>
+              <p className="text-sm">{plan.user_limit}</p>
             </div>
             <div className="flex justify-between px-5 mt-2">
-              <p className="text-base font-light">Users</p>
-              <p className="text-sm">1</p>
+              <p className="text-base font-light">Additional user fee</p>
+              <p className="text-sm">{plan.additional_user_fee ? `${plan.additional_user_fee} / Month` : 'Not applicable'}</p>
             </div>
-
-            <button className="mt-10 bg-blue-500 px-4 py-2 text-white rounded text-sm hover:bg-blue-400 hover:text-gray-200">Get Started</button>
+            <button className="mt-10 bg-blue-500 px-4 py-2 text-white rounded text-sm hover:bg-blue-400 hover:text-gray-200" onClick={()=>setSelectedPlan(plan)}>Get Started</button>
           </div>
-        }
-        
-
-        <div className="rounded shadow-md p-4">
-          <h1 className="text-3xl">Start up</h1>
-          <h2 className="text-2xl mt-4">$6 / {payPerMonth ? 'month' : 'year'}</h2>
-          <div className="flex justify-between px-5 mt-10">
-            <p className="text-base font-light">Template</p>
-            <p className="text-sm">Unlimited</p>
-          </div>
-          <div className="flex justify-between px-5 mt-2">
-            <p className="text-base font-light">Users</p>
-            <p className="text-sm">20</p>
-          </div>
-          <div className="flex justify-between px-5 mt-2">
-            <p className="text-base font-light">Additional user</p>
-            <p className="text-sm">+6$</p>
-          </div>
-
-          <button className="mt-10 bg-blue-500 px-4 py-2 text-white rounded text-sm hover:bg-blue-400 hover:text-gray-200">Get Started</button>
-        </div>
-
-        <div className="rounded shadow-md p-4">
-          <h1 className="text-3xl">Enterprise</h1>
-          <h2 className="text-2xl mt-4">$6 / {payPerMonth ? 'month' : 'year'}</h2>
-          <div className="flex justify-between px-5 mt-10">
-            <p className="text-base font-light">Template</p>
-            <p className="text-sm">Unlimited</p>
-          </div>
-          <div className="flex justify-between px-5 mt-2">
-            <p className="text-base font-light">Users</p>
-            <p className="text-sm">20</p>
-          </div>
-          <div className="flex justify-between px-5 mt-2">
-            <p className="text-base font-light">Addition users</p>
-            <p className="text-sm">+5$</p>
-          </div>
-
-          <button className="mt-10 bg-blue-500 px-4 py-2 text-white rounded text-sm hover:bg-blue-400 hover:text-gray-200">Get Started</button>
-        </div>
+        ))}
       </div>
-
-      <BillForm />
+      { selectedPlan && <BillForm selectedPlan={selectedPlan} subscriptionType={payPerMonth ? 'monthly' : 'yearly'}/> }
     </>
   )
 }
